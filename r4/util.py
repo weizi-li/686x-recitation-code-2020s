0bab0f0ae34c9da3071f4a48127815a79e31daab
weizili <weizili.cs@gmail.com>
2020-02-26 12:59:55 -0500

    edited r4

--- r4/util.py
+++ r4/util.py
@@ -4,16 +4,6 @@ from matplotlib import pyplot as plt
 from sklearn.datasets import make_regression
 
 def get_data():
-    # X, y, coefficients = make_regression(
-    #     n_samples=50,
-    #     n_features=1,
-    #     n_informative=1,
-    #     n_targets=1,
-    #     noise=5,
-    #     coef=True,
-    #     random_state=1
-    # )
-
     ### generate X and y
     X = np.array([i*np.pi/180 for i in range(1,150,2)])
     y = np.sin(X) + np.random.normal(0,0.15,len(X))
@@ -35,6 +25,7 @@ def compute_error(pred, actual):
     err = np.sum(np.power(pred - actual, 2))
     return err
 
+
 def compute_train_test_error(X_train, y_train, X_test, y_test, coef, intercept):
     pred_train = coef*X_train + intercept
     pred_test = coef*X_test + intercept
@@ -43,18 +34,24 @@ def compute_train_test_error(X_train, y_train, X_test, y_test, coef, intercept):
     print("Training Error: " + str(train_err))
     print("Test Error: " + str(test_err))
 
-def plot_fit(X_train, y_train, X_test, y_test, coef, intercept, color):
-    plt.scatter(X_train, y_train)
-    plt.scatter(X_test, y_test)
-    plt.plot(X_train, coef*X_train + intercept, c=color)
-    plt.plot(X_test, coef*X_test + intercept, c=color)
+
+def plot_fit(X_train, y_train, X_test, y_test, coef, intercept, color, line_label):
+    X = np.concatenate((X_train,X_test),axis=0)
+    y = np.concatenate((y_train,y_test),axis=0)
+    plt.scatter(X_train, y_train, label="training set")
+    plt.scatter(X_test, y_test, label="test set")
+    plt.plot(X, coef*X + intercept, c=color, label=line_label)
+    plt.xlabel('X')
+    plt.ylabel('y')
+    plt.legend()
+
 
 def ridge_regression(X_train, y_train, alpha):
-    # ### append a column of ones to X
-    # x0 = np.ones((X_train.shape[0], 1))
-    # X_train = np.append(X_train, x0, axis=1)
+    ### own implementation of the ridge regression
+    ### Usage:
+    ### my_rr_coef = ridge_regression(X_train, y_train, alpha)
+    ### print(my_rr_coef) # this should match rr.coef_ above, if fit_intercept is set to False
     I = np.identity(X_train.shape[1])
     w = np.dot(np.dot(np.linalg.inv(np.dot(X_train.T, X_train) + alpha * I), X_train.T), y_train)
     coef = w[0]
-    #intercept = w[1]
-    return coef #, intercept
+    return coef
